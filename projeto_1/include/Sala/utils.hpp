2dfdc3300adfa5d1c336e06aa03788a6c36d15a6
Thiago M <rpj134@gmail.com>
2024-10-13 15:12:30 -0300

    Update lampada aumentando com o brilho (incompleto)

--- projeto_1/include/Sala/utils.hpp
+++ projeto_1/include/Sala/utils.hpp
@@ -3,12 +3,17 @@
 
 enum indiceSensor
 {
-  LUMINOSIDADE = 0 
+  LUMINOSIDADE  = 0,
+  UMIDADE       = 1,
+  TEMPERATURA   = 2
 };
 
 enum indiceAtuador
 {
-  LAMPADA = 0
+  LAMPADA         = 0,
+  UMIDIFICADOR    = 1,
+  DESUMIDIFICADOR = 2,
+  VENTILADOR      = 3,
 };
 
 #endif 
--- projeto_1/src/Sala/Sala.cpp
+++ projeto_1/src/Sala/Sala.cpp
@@ -2,13 +2,24 @@
 #include "Sala/utils.hpp"
 
 #include "Atuadores/Lampada/Lampada.hpp"
+#include "Atuadores/Desumidificador/Desumidificador.hpp"
+#include "Atuadores/Umidificador/Umidificador.hpp"
+#include "Atuadores/Ventilador/Ventilador.hpp"
+
 #include "Sensores/Luminosidade/Luminosidade.hpp"
+#include "Sensores/Temperatura/Temperatura.hpp"
+#include "Sensores/Umidade/Umidade.hpp"
 
 Sala::Sala()
 {
   atuadores.push_back(new Lampada(12));
-  
+  atuadores.push_back(new Umidificador(12));
+  atuadores.push_back(new Desumidificador(12));
+  atuadores.push_back(new Ventilador(12));
+
   sensores.push_back(new Luminosidade(7));
+  sensores.push_back(new Umidade(6));
+  sensores.push_back(new Temperatura(5));
 }
 
 Sala::~Sala()
@@ -17,7 +28,11 @@ Sala::~Sala()
 
 void Sala::atualiza()
 {
-  if(((Luminosidade *)sensores[LUMINOSIDADE])->estaClaro()) ((Lampada *)atuadores[LAMPADA])->setBrilho(255);
-  else ((Lampada *)atuadores[LAMPADA])->setBrilho(255);
-
+  if(((Luminosidade *)sensores[LUMINOSIDADE])->estaClaro())
+  {
+    std::cout << ((Luminosidade *)sensores[LUMINOSIDADE])->getValor() << std::endl;
+    ((Lampada *)atuadores[LAMPADA])->setBrilho(255);
+  }
+  else ((Lampada *)atuadores[LAMPADA])->setBrilho(0);
+  
 }
